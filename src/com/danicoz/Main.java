20344385de7626e566923ce819fca3520ee8487b
刘永坚 <1421537902@qq.com>
2019-01-23 14:01:22 +0800

    测试分支2

--- src/com/danicoz/Main.java
+++ src/com/danicoz/Main.java
@@ -29,6 +29,7 @@ public class Main {
 		main.excute();
 		System.out.println("测试 Git 日志！");
 		System.out.println("测试 Git 分支日志");
+		System.out.println("测试 Git 分支日志2");
 
 		/*
 		 * 通过钩子函数来清空资源，主要关闭连接，释放列表等。
